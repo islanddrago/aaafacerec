9c4234423466978bd2dbf31807b88ad278b7a973
islanddrago <caseykinnamon@my.unt.edu>
2019-01-26 17:46:44 -0600

    image proccesing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import Webcam from "react-webcam";
+import Webcame from './Webcame';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,22 +9,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Navbar title={'Test Title'} />
+        <Webcame></Webcame>
+        
+      
+            </div>
+           
     );
   }
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            json: undefined,
+        };
+    }
+
+    fetchData() {
+        let promise = fetch('http://localhost:5000/test');
+        promise.then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+            this.setState({ json });
+        });
+        promise.catch((error) => {
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <div style={{ position: 'absolute', width: '100%', height: '100px', top: 0, left: 0 }}>
-                <h1>Header</h1>
+                <h1>hiiii</h1>
+                <p>{this.props.title}</p>
+                <button onClick={() => {
+                    this.fetchData();
+                }}>Click me</button>
+                <p>{JSON.stringify(this.state.json)}</p>
             </div>
         );
     }
