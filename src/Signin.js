246cabf8282e95bbb7d7366da2a63d2c45f3951c
Natalie <nataliedrewfs@gmail.com>
2019-01-27 01:48:00 -0600

    i did things

---
+++ src/Signin.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+signinClicked(){
+    return(
+      <div>
+        <h1>Test</h1>;
+      </div>
+    );
+  }
+  export default Signin;
---
+++ src/Signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function signupClicked(){
+    return(
+      <div>
+        <h1>Test</h1>;
+      </div>
+    );
+    console.log('test')
+  }
+  export default Signup;
