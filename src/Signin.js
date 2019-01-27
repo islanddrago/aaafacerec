e03b26969ac88176432b435abc5708482f34ff8c
Natalie <nataliedrewfs@gmail.com>
2019-01-27 01:48:20 -0600

    message

--- src/Signin.js
+++ src/Signin.js
@@ -1,10 +1,2 @@
 import React, { Component } from 'react';
 
-signinClicked(){
-    return(
-      <div>
-        <h1>Test</h1>;
-      </div>
-    );
-  }
-  export default Signin;
--- src/Signup.js
+++ src/Signup.js
@@ -1,11 +1,2 @@
 import React, { Component } from 'react';
 
-function signupClicked(){
-    return(
-      <div>
-        <h1>Test</h1>;
-      </div>
-    );
-    console.log('test')
-  }
-  export default Signup;
